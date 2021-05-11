d5e30c3cf0c95063c66fec5c25f2d7b872152a7b
Milad <khoshkaran.milad@gmail.com>
2021-05-11 18:49:29 +0200

    isNan fixed

--- JavaScript/11.5 - logType.js
+++ JavaScript/11.5 - logType.js
@@ -1,28 +1,38 @@
 function logType(value) {
     if (typeof value === "undefined") {
         console.log("undefined!");
+        return;
     } else if (value === null) {
         console.log("null!");
+        return;
+    } else if (isNaN(value) && typeof value === "number") {
+        console.log("not a number!");
+        return;
     } else if (typeof value === "number") {
         console.log("number!");
+        return;
     } else if (typeof value === "bigint") {
         console.log("bigint!");
-    } else if (isNaN(value)) {
-        console.log("not a number!");
+        return;
     } else if (typeof value === "string") {
         console.log("string!");
+        return;
     } else if (typeof value === "boolean") {
         console.log("boolean!");
+        return;
     } else if (typeof value === "function") {
         console.log("function!");
+        return;
     } else if (Array.isArray(value)) {
         console.log("array!");
+        return;
     } else if (typeof value === "object") {
         console.log("object!");
+        return;
     } else {
         console.log("I have no idea!");
+        return;
     }
 }
 
-var e = null;
-logType(e);
+logType(NaN);
---
+++ JavaScript/forInLoop.js
@@ -0,0 +1,12 @@
+var a = {
+    Berlin: "Germany",
+    Paris: "France",
+    "New York": "USA",
+};
+
+var b = {};
+
+for (var p in a) {
+}
+console.log(a);
+console.log(b);
