de2748976759fc9bf77e9e6891150756c448db5d
Milad <khoshkaran.milad@gmail.com>
2021-05-12 16:21:52 +0200

    exercise 3 solved and refactored again

--- JavaScript/12.5/exercise3.js
+++ JavaScript/12.5/exercise3.js
@@ -1,14 +1,15 @@
 function checkNumber(num) {
+    const MAX_LIMIT = 1000000;
     if (num <= 0 || typeof num !== "number") {
         return "ERROR";
-    } else if (num >= 1000000) {
+    } else if (num >= MAX_LIMIT) {
         return num;
     }
     var multplyCounter = 0;
     do {
         num *= 10;
         multplyCounter++;
-    } while (num < 1000000);
+    } while (num < MAX_LIMIT);
     return `new number is ${num}, times multiplied: ${multplyCounter}`;
 }
 checkNumber(-12);
