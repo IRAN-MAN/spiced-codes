efd9afcc7fa9a548d08c6ee3b723f45212fcdb82
Milad <khoshkaran.milad@gmail.com>
2021-05-12 16:14:15 +0200

    exercise 3 solved and tested

--- JavaScript/12.5/exercise3.js
+++ JavaScript/12.5/exercise3.js
@@ -1,9 +1,7 @@
 function checkNumber(num) {
     if (num < 0 || num === 0 || typeof num !== "number") {
-        // console.log("Error!");
         return "ERROR";
     } else if (num >= 1000000) {
-        // console.log(num);
         return num;
     }
     var multplyCounter = 0;
@@ -11,8 +9,10 @@ function checkNumber(num) {
         num *= 10;
         multplyCounter++;
     } while (num < 1000000);
-    // console.log(num, "times multiplied:", multplyCounter);
-    return `${num} + "times multiplied: " + ${multplyCounter}`;
+    return `new number is ${num}, times multiplied: ${multplyCounter}`;
 }
-
+checkNumber(-12);
+checkNumber(0);
+checkNumber("String");
 checkNumber(1000000);
+checkNumber(25);
