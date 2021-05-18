73c41abebf57abc340d74c2b867203b79ffa8f51
Milad <khoshkaran.milad@gmail.com>
2021-05-18 18:38:12 +0200

    exercise 2 refactored

--- JavaScript/DOM/18.5/exe2returnByClassName.js
+++ JavaScript/DOM/18.5/exe2returnByClassName.js
@@ -4,10 +4,20 @@
 
 function returnByClassName(className) {
     // var elements = [...document.getElementsByClassName(className)];
-    var elements = Array.from(document.getElementsByClassName(className));
+    // var elements = Array.from(document.getElementsByClassName(className));
+    // console.log(Array.isArray(elements)); // logs true for both cases above
+    // return elements;
 
-    console.log(Array.isArray(elements)); // logs true for both cases above
-    return elements;
+    // seems like some older browsers don't support Array.from() and the other solution.
+    // i refactor the code as below:
+
+    var classArray = [];
+    var elements = document.getElementsByClassName(className);
+    for (var i = 0; i < elements.length; i++) {
+        classArray.push(elements[i]);
+    }
+    console.log(Array.isArray(classArray));
+    return classArray;
 }
 
 returnByClassName("change");
