5772d6c0883cecd50f79cbdf4f57fb6e8a0cc201
Milad <khoshkaran.milad@gmail.com>
2021-05-19 18:35:48 +0200

    Bonus Ticker done

--- Ticker/script.js
+++ Ticker/script.js
@@ -1,8 +1,11 @@
+// Top Ticker
+
 (function () {
     var topHeadlines = document.getElementById("headlines-top");
     var topLinks = topHeadlines.getElementsByClassName("top-links");
     var left = topHeadlines.offsetLeft;
     var animationId;
+    var SPEED = 2;
 
     function addHoverEffectToLinks() {
         for (var i = 0; i < topLinks.length; i++) {
@@ -23,7 +26,7 @@
     addHoverEffectToLinks();
     moveHeadlines();
     function moveHeadlines() {
-        left--;
+        left -= SPEED;
         if (left <= -topLinks[0].offsetWidth) {
             left += topLinks[0].offsetWidth;
             topHeadlines.appendChild(topLinks[0]);
@@ -34,27 +37,45 @@
     }
 })();
 
+// Bottom Ticker
+
 (function () {
     var bottomHeadlines = document.getElementById("headlines-bottom");
-    // var bottomLinks = bottomHeadlines.getElementsByClassName("bottom-links");
+    var bottomLinks = bottomHeadlines.getElementsByClassName("bottom-links");
+    var SPEED = 2;
+    var animationId;
     var right =
         window.innerWidth -
         (bottomHeadlines.offsetLeft + bottomHeadlines.offsetWidth);
+
+    function addHoverEffectToLinks() {
+        for (var i = 0; i < bottomLinks.length; i++) {
+            var link = bottomLinks[i];
+            link.addEventListener("mouseenter", function () {
+                this.style.textDecoration = "underline";
+                this.style.color = "blue";
+                cancelAnimationFrame(animationId);
+            });
+            link.addEventListener("mouseleave", function () {
+                this.style.textDecoration = "";
+                this.style.color = "";
+                moveBottomHeadlines();
+            });
+        }
+    }
+
+    addHoverEffectToLinks();
     moveBottomHeadlines();
     function moveBottomHeadlines() {
         var lastChild = bottomHeadlines.lastElementChild;
-        right--;
+        right -= SPEED;
 
         if (right <= lastChild.offsetWidth) {
             right += lastChild.offsetWidth;
-            bottomHeadlines.removeChild(bottomHeadlines.lastChild);
-            bottomHeadlines.insertBefore(
-                bottomHeadlines.lastChild,
-                bottomHeadlines.firstChild
-            );
+            bottomHeadlines.insertBefore(lastChild, bottomHeadlines.firstChild);
         }
         bottomHeadlines.style.right = right + "px";
 
-        requestAnimationFrame(moveBottomHeadlines);
+        animationId = requestAnimationFrame(moveBottomHeadlines);
     }
 })();
