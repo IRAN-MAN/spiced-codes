9d6b693e2c88870f62387180283c1996a2a9e874
Milad <khoshkaran.milad@gmail.com>
2021-05-19 16:31:00 +0200

    exercise 4 changeNestedColor - strange behavior

---
+++ JavaScript/DOM/19.5/exe4changeNestedColor.js
@@ -0,0 +1,27 @@
+/*
+Make a page that has on it an element that is 200px by 200px in size and has a solid background color. 
+Nest within that element another element that is 50px by 50px in size and has a different solid background 
+color. When the user clicks on the outer element its background color should change to a randomly selected 
+color. However, if the user clicks on the inner element, the inner element's background color should 
+change to a randomly selected background color but the outer element's background color should not 
+change at all.
+*/
+
+var outsideBox = document.getElementById("exe4");
+var insideBox = document.getElementById("inside-box");
+
+outsideBox.addEventListener("click", outsideBoxHandler);
+
+function outsideBoxHandler(event) {
+    event.target.style.backgroundColor = getRandomColor();
+}
+
+function getRandomNumber() {
+    var randomNumber = Math.floor(Math.random() * 255);
+    return randomNumber;
+}
+
+function getRandomColor() {
+    var randomColor = `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
+    return randomColor;
+}
