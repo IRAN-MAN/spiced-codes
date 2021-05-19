2d346fd992819bb88794b36c2e346d84e7126487
Milad <khoshkaran.milad@gmail.com>
2021-05-19 09:20:31 +0200

    failure attempt to make the bonus ticker

--- Ticker/script.js
+++ Ticker/script.js
@@ -1,16 +1,34 @@
 (function () {
-    var headlines = document.getElementById("headlines");
-    var links = headlines.getElementsByTagName("A");
-    var left = headlines.offsetLeft;
+    var topHeadlines = document.getElementById("headlines-top");
+    var topLinks = topHeadlines.getElementsByClassName("top-links");
+    var left = topHeadlines.offsetLeft;
     moveHeadlines();
     function moveHeadlines() {
         left--;
-        if (left <= -links[0].offsetWidth) {
-            left += links[0].offsetWidth;
-            headlines.appendChild(links[0]);
+        if (left <= -topLinks[0].offsetWidth) {
+            left += topLinks[0].offsetWidth;
+            topHeadlines.appendChild(topLinks[0]);
         }
-        headlines.style.left = left + "px";
+        topHeadlines.style.left = left + "px";
 
         requestAnimationFrame(moveHeadlines);
+    }
+})();
+
+(function () {
+    var bottomHeadlines = document.getElementById("headlines-bottom");
+    var bottomLinks = bottomHeadlines.getElementsByClassName("bottom-links");
+    var right = bottomHeadlines.offsetRight; // guess the problem lays here
+    console.log(right);
+    moveBottomHeadlines();
+    function moveBottomHeadlines() {
+        right--;
+        if (right <= -bottomLinks[0].offsetWidth) {
+            right += bottomLinks[0].offsetWidth;
+            bottomHeadlines.appendChild(bottomLinks[0]);
+        }
+        bottomHeadlines.style.right = right + "px";
+
+        requestAnimationFrame(moveBottomHeadlines);
     }
 })();
