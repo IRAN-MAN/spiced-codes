a84ee41347fd330829c3670dce6d7c04e7734e42
Milad <khoshkaran.milad@gmail.com>
2021-05-19 13:18:34 +0200

    exercise1 make center solved

---
+++ JavaScript/DOM/19.5/exe1makeCenter.js
@@ -0,0 +1,15 @@
+/* 
+Make a page that has on it an element that is 100px by 100px in size, 
+has absolute positioning, and has a solid background color. 
+Add an event handler that makes this box center itself directly under the user's 
+mouse pointer as it is moved across the screen.
+*/
+
+var div = document.getElementById("div-center");
+
+document.addEventListener("mousemove", documentHandler);
+
+function documentHandler(event) {
+    div.style.top = event.clientY - 50 + "px";
+    div.style.left = event.clientX - 50 + "px";
+}
