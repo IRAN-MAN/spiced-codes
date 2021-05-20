766375d2f12118cbe344c7040f0cdf55480d48c4
Milad <khoshkaran.milad@gmail.com>
2021-05-20 11:54:35 +0200

    Canvas Bonus - evenListener added

--- JavaScript/Canvas/canvas.js
+++ JavaScript/Canvas/canvas.js
@@ -1,22 +1,42 @@
-var ctx = document.getElementById("canv").getContext("2d");
+var ctxS = document.getElementById("canv-small").getContext("2d");
+var ctxB = document.getElementById("canv-big").getContext("2d");
+var smallConvas = document.getElementById("canv-small");
 
-ctx.beginPath();
-ctx.arc(150, 50, 40, 0, Math.PI * 2, true); // head
-ctx.moveTo(135, 43);
-ctx.arc(135, 43, 5, 0, Math.PI * 2, true); // left eye
-ctx.moveTo(165, 43);
-ctx.arc(165, 43, 5, 0, Math.PI * 2, true); // right eye
-ctx.moveTo(125, 55);
-ctx.arc(151, 55, 25, 0, Math.PI, false); // smile
-ctx.moveTo(150, 89);
-ctx.lineTo(150, 120); // neck
-ctx.lineTo(70, 90); // left hand
-ctx.moveTo(150, 120);
-ctx.lineTo(220, 90); // right hand
-ctx.moveTo(150, 120);
-ctx.lineTo(150, 185); // body
-ctx.moveTo(150, 185);
-ctx.lineTo(78, 245); // left leg
-ctx.moveTo(150, 185);
-ctx.lineTo(220, 245); // right leg
-ctx.stroke();
+ctxS.beginPath();
+ctxS.arc(110, 50, 40, 0, Math.PI * 2, true); // head
+ctxS.moveTo(95, 43);
+ctxS.arc(95, 43, 5, 0, Math.PI * 2, true); // left eye
+ctxS.moveTo(125, 43);
+ctxS.arc(125, 43, 5, 0, Math.PI * 2, true); // right eye
+ctxS.moveTo(85, 55);
+ctxS.arc(111, 55, 25, 0, Math.PI, false); // smile
+ctxS.moveTo(110, 89);
+ctxS.lineTo(110, 120); // neck
+ctxS.lineTo(30, 90); // left hand
+ctxS.moveTo(110, 120);
+ctxS.lineTo(180, 90); // right hand
+ctxS.moveTo(110, 120);
+ctxS.lineTo(110, 185); // body
+ctxS.moveTo(110, 185);
+ctxS.lineTo(38, 245); // left leg
+ctxS.moveTo(110, 185);
+ctxS.lineTo(180, 245); // right leg
+ctxS.stroke();
+ctxS.closePath();
+
+ctxB.drawImage(smallConvas, 40, 40);
+
+document.addEventListener("keydown", keydownHandler);
+
+function keydownHandler(event) {
+    if (event.keyCode == 39) {
+        ctxB.clearRect(0, 0, 300, 300);
+        ctxB.drawImage(smallConvas, 50, 40);
+        ctxB.clearRect(0, 0, 300, 300);
+        ctxB.drawImage(smallConvas, 60, 40);
+    }
+}
+/* 
+    ctx.drawImage(img, 100, 250);
+    ctx.clearRect(0,0,50,50)
+*/
