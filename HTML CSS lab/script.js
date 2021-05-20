5a2c3c986f2dbe11f9b457945db0a87a5259c8a6
Milad <khoshkaran.milad@gmail.com>
2021-05-20 17:20:11 +0200

    backdrop added with toggle

--- HTML CSS lab/script.js
+++ HTML CSS lab/script.js
@@ -1,13 +1,16 @@
 var elementsThatToggle = document.querySelectorAll(".can-toggle-nav");
 var nav = document.getElementById("nav");
 var hamburger = document.getElementById("hamburger");
+var backDrop = document.getElementById("back-drop");
 
 hamburger.addEventListener("click", function () {
     nav.classList.add("open");
+    backDrop.classList.add("backdrop");
 });
 
 elementsThatToggle.forEach(function (element) {
     element.addEventListener("click", function () {
         nav.classList.toggle("open");
+        backDrop.classList.toggle("backdrop");
     });
 });
