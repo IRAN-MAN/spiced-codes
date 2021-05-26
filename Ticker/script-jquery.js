0c88ebf8bca2678460380c4b60368f1a89480a8a
Milad <khoshkaran.milad@gmail.com>
2021-05-26 19:34:51 +0200

    Ticker refactored with jQuery - top headlines done

---
+++ Ticker/script-jquery.js
@@ -0,0 +1,35 @@
+(function () {
+    var $topHeadlines = $("#headlines-top");
+    var $topLinks = $topHeadlines.find(".top-links");
+    var $left = $topHeadlines.offset().left;
+    var animationId;
+    var SPEED = 3;
+
+    moveHeadlines();
+    addHoverEffectToLinks();
+
+    function moveHeadlines() {
+        $left -= SPEED;
+        var $topLinks = $topHeadlines.find(".top-links");
+        if ($left <= -parseInt($($topLinks.eq(0)).css("width").slice(0, -2))) {
+            $left += parseInt($($topLinks.eq(0)).css("width").slice(0, -2));
+            $topLinks.eq(0).appendTo($topHeadlines);
+        }
+        $topHeadlines.css({ left: $left + "px" });
+        animationId = requestAnimationFrame(moveHeadlines);
+    }
+
+    function addHoverEffectToLinks() {
+        $topLinks.each(function () {
+            $(this).hover(mouseIn, mouseOut);
+        });
+        function mouseIn() {
+            cancelAnimationFrame(animationId);
+            $(this).css({ "text-decoration": "underline", color: "blue" });
+        }
+        function mouseOut() {
+            $(this).css({ "text-decoration": "", color: "" });
+            moveHeadlines();
+        }
+    }
+})();
