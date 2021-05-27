a5a5c7b61d4189d5cbceda086169842f51d24edf
Milad <khoshkaran.milad@gmail.com>
2021-05-27 13:01:39 +0200

    Modal box added

--- HTML CSS lab/script.js
+++ HTML CSS lab/script.js
@@ -2,6 +2,19 @@ var elementsThatToggle = document.querySelectorAll(".can-toggle-nav");
 var nav = document.getElementById("nav");
 var hamburger = document.getElementById("hamburger");
 var backDrop = document.getElementById("back-drop");
+var $modal = $(".modal-container");
+var $modalButton = $(".modal-button");
+var $body = $(document.body);
+
+setTimeout(function () {
+    $modal.css({ display: "flex" });
+    $body.addClass("no-scroll");
+}, 1000);
+
+$modalButton.on("click", function () {
+    $modal.css({ display: "none" });
+    $body.removeClass("no-scroll");
+});
 
 hamburger.addEventListener("click", function () {
     nav.classList.add("open");
