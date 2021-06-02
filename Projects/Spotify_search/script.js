7d83b4028a95d472c1feb1af61db0aa6c1da0098
Milad <khoshkaran.milad@gmail.com>
2021-06-02 23:19:04 +0200

    Spotify Search - moreButton implemented

--- Projects/Spotify_search/script.js
+++ Projects/Spotify_search/script.js
@@ -4,13 +4,41 @@ $(function () {
     var $form = $("form");
     var $resultList = $(".result-list");
     var $resultTitle = $(".result-title");
-    var moreButton = $(".load-more-button");
+    var $moreButton = $(".load-more-button");
 
     var nextURL = null;
     var searchValue = null;
     var optionValue = null;
 
     $form.on("submit", submitFormHandler);
+
+    $moreButton.on("click", moreButtomClickHandler);
+
+    function moreButtomClickHandler() {
+        var replacedURL = replaceURLName(nextURL);
+        $.ajax({
+            url: replacedURL,
+            success: function (moreData) {
+                var more = extractInfoFromData(moreData);
+                var moreDataItems = more.items;
+                moreDataItems.forEach(function (result) {
+                    var $listItem = $("<li class=individual-result></li>");
+                    var $link = $("<a></a>");
+                    var $image = $("<img></img>");
+                    var linkURL = result.external_urls.spotify;
+                    $link.attr("href", linkURL).html(result.name);
+
+                    result.images.length
+                        ? $image.attr("src", result.images[0].url)
+                        : $image.attr("src", "https://via.placeholder.com/150");
+
+                    $link.appendTo($listItem);
+                    $image.appendTo($listItem);
+                    $listItem.appendTo($resultList);
+                });
+            },
+        });
+    }
 
     function submitFormHandler(event) {
         event.preventDefault();
@@ -23,19 +51,40 @@ $(function () {
                 type: optionValue,
             },
             success: function (data) {
-                var results = extractItemsFromData(data);
-                showResultTitle(results, searchValue);
-                renderResults(results);
-                console.log(results);
+                var proccessedData = extractInfoFromData(data);
+                showResultTitle(proccessedData, searchValue);
+                renderResults(proccessedData.items);
+                nextURL = proccessedData.next;
+
+                // console.log(proccessedData.next);
             },
         });
     }
 
-    function extractItemsFromData(data) {
+    function replaceURLName(spotifyURL) {
+        return spotifyURL.replace("https://api.spotify.com/v1/search", API_URL);
+    }
+
+    function extractInfoFromData(data) {
         if (data.artists) {
-            return data.artists.items;
+            return data.artists;
         }
-        return data.albums.items;
+        return data.albums;
+    }
+
+    function showResultTitle(results, searchValue) {
+        if (results.length == 0) {
+            $resultTitle
+                .html(
+                    "Sorry, We could not find anything for " +
+                        searchValue.toUpperCase()
+                )
+                .css({ color: "red" });
+        } else {
+            $resultTitle.html(
+                "Showing results for: " + searchValue.toUpperCase()
+            );
+        }
     }
 
     function renderResults(results) {
@@ -54,21 +103,7 @@ $(function () {
             $link.appendTo($listItem);
             $image.appendTo($listItem);
             $listItem.appendTo($resultList);
+            $moreButton.show();
         });
-    }
-
-    function showResultTitle(results, searchValue) {
-        if (results.length == 0) {
-            $resultTitle
-                .html(
-                    "Sorry, We could not find anything for " +
-                        searchValue.toUpperCase()
-                )
-                .css({ color: "red" });
-        } else {
-            $resultTitle.html(
-                "Showing results for: " + searchValue.toUpperCase()
-            );
-        }
     }
 });
